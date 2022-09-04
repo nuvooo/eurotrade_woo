0e0e6a2ac56ff49c399e4f7af575074c9f6e4aae
nuVooo <sebastian.angelone@googlemail.com>
2022-09-04 22:48:57 +0200

    fix product error

--- packages/ecommerce-ui/ProductBox/__test__/ProductBox.cy.tsx
+++ packages/ecommerce-ui/ProductBox/__test__/ProductBox.cy.tsx
@@ -2,7 +2,7 @@ import ProductBox from '..'
 
 describe('<ProductBox>', () => {
   it('render correctly', () => {
-    cy.mount(<ProductBox price={12.99} name={'ProductBox'} />)
+    cy.mount(<ProductBox href="/" price={12.99} name={'ProductBox'} />)
     cy.contains('ProductBox')
     // Render Formated Price correctly
     cy.contains('12,99 €')
